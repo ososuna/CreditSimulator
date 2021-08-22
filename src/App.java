12053d5e902bd035e5dc2fd02ab5800cbd3fe8f3
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-08-22 13:08:11 -0500

    Create Form Graphics

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import classes.Graphics;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Graphics g = new Graphics();
     }
 }
